e88ea4ac78261d29cc287afc0c36235f035aacdc
Abhinav Shinow <abhinavshinow@gmail.com>
2021-07-15 18:47:26 +0530

    .

--- Content/app.py
+++ Content/app.py
@@ -4,17 +4,8 @@ from models import mongo
 def create_app():
     app=Flask(__name__)
 
+    app.config['MONGO_URI'] = 'mongodb+srv://abhinav:Password@10@cluster0.axu85.mongodb.net/To_Do_App?retryWrites=true&w=majority'
+
     mongo.init_app(app)
     return app
-mongodb+srv://abhinav:Password@10@cluster0.axu85.mongodb.net/To_Do_App?retryWrites=true&w=majority
 
-@app.route("/")
-def home():
-    return "Pass Value"
-
-@app.route("/<name>")              
-def hello_name(name):             
-    return "Hello "+ name 
-
-if __name__ == "__main__":        
-    app.run()
