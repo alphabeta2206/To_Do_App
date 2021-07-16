8a285536c7fecb658057b394ed9740ed1e87a02b
Abhinav Shinow <abhinavshinow@gmail.com>
2021-07-16 19:09:18 +0530

    .

--- Content/app.py
+++ Content/app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('home.html')
+   
+@app.route('/dashboard')
+def dashboard():
+   return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
    app.run()
